ddeb99cba4f186db8a86e00d8709591caaa66185
lino089 <marvellino089@gmail.com>
2026-09-02 22:07:27 +0700

    OOP

--- JavaScript/OOP/Kuis_Coding.js
+++ JavaScript/OOP/Kuis_Coding.js
@@ -30,3 +30,30 @@
 
 // Tulis kode di bawah ini
 
+class Animal {
+    constructor (name, age, isMammal){
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    }
+}
+
+class Rabbit extends Animal{
+    eat(){
+        console.log(`${this.name} sedang makan!`);
+    }
+
+    isMammal = true;
+}
+
+class Eagle extends Animal{
+    fly() {
+        console.log(`${this.name} sedang terbang!`);
+    }
+
+    isMammal = false;
+}
+
+const myRabbit = new Rabbit('Labi', 2);
+const myEagle = new Eagle('Elo', 4);
+
